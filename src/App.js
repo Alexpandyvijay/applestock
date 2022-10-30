5653248cc661f4e6b82fe42da4219875efb6a823
alexpandy <alexpandyvijay@gmail.com>
2022-10-30 12:34:45 +0530

    frist publish of apple stock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useEffect,useState} from 'react';
 import './App.css';
 
 function App() {
+  const [docs, setDocs]=useState([]);
+
+  useEffect(()=>{
+    const fetchData = async () =>{
+      await fetch('https://api.twelvedata.com/time_series?apikey=59011778f4f84cfc81dc97a9e904e9ae&interval=1month&symbol=AAPL')
+      .then((data)=>(data.json()))
+      .then((data)=>{setDocs(data.values)})
+      .catch((err)=>(console.log(err)));
+    }
+    fetchData();
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+        <tr>
+            <th>Date</th>
+            <th>Open</th>
+            <th>High</th>
+            <th>Low</th>
+            <th>Close</th>
+            <th>Volume</th>
+          </tr>
+        </thead>
+        <tbody>
+          {docs.map((value,key)=>{
+            return (<tr key={key}>
+              <td>{value.datetime}</td>
+              <td>{value.open}</td>
+              <td>{value.high}</td>
+              <td>{value.low}</td>
+              <td>{value.close}</td>
+              <td>{value.volume}</td>
+            </tr>);
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
